d80bf98e917cfd13f3289e865378a760f078c49f
DhratiThepadia <dhratithepedia@gmail.com>
2022-03-13 15:41:57 +0530

    orderDetail component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Headers from './components/header';
+import { Switch,Route } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { Autocomplete, TextField } from '@mui/material';
 import React from 'react';
+import OrderDetail from './components/orderDetail';
 const App = ()=> {
   const state = useSelector((state)=>state)
   const dispatch = useDispatch();
   return (
     <React.Fragment>
       <Headers />
+        <Switch>
+          <Route exact path={"/OrderDetails"} component={OrderDetail}/>
+        </Switch>
     </React.Fragment>
     
   )
---
+++ src/components/orderDetail.js
@@ -0,0 +1,118 @@
+import React from 'react'
+import { Card, CardBody, Col, FormGroup, Input, Label, Row } from 'reactstrap';
+import {TextField ,Autocomplete,Button} from "@mui/material"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+
+const OrderDetail = () => {
+  return (
+    <div>
+        <Row className="orderDetailRow">
+        <Col sm="8">
+            <h3>
+            Event Details
+            </h3>
+            <Card>
+            <CardBody>
+                <Row className="eventDetailRow">
+                    <Col sm="6">
+                        <div>Name</div>
+                        <TextField 
+                        className="form-control"
+                        id="outlined-basic" 
+                        label="Name" 
+                        variant="outlined" 
+                        />
+                    </Col>
+                    <Col sm="6">
+                    <div>Contact Number</div>
+                    <TextField
+                    className="form-control" 
+                        id="outlined-basic" 
+                        label="Contact Number" 
+                        variant="outlined" 
+                        />
+                    </Col>
+                </Row> 
+                <Row className="eventDetailRow">
+                    <Col sm="6">
+                    <div>Event Name</div>
+                        <TextField 
+                        className="form-control"
+                        id="outlined-basic" 
+                        label="Event Name" 
+                        variant="outlined" 
+                        />
+                    </Col>
+                    <Col sm="6">
+                    <div>Event Location</div>
+                    <TextField 
+                    className="form-control"
+                        id="outlined-basic" 
+                        label="Event Location" 
+                        variant="outlined" 
+                        />
+                    </Col>
+                </Row> 
+                <Row className="eventDetailRow">
+                    <Col sm="6">
+                    <div>Event Name</div>
+                        <TextField 
+                        className="form-control"
+                        id="outlined-basic" 
+                        label="Event Date" 
+                        variant="outlined" 
+                        />
+                    </Col>
+                    <Col sm="6">
+                    <div>Event Name</div>
+                    <Autocomplete
+                    className="form-control"
+                    disablePortal
+                    id="combo-box-demo"
+                    options={[]}
+                    sx={{ width: 300 }}
+                    renderInput={(params) => <TextField {...params} label="Select Delvery Mode" />}
+/>
+                    </Col>
+                </Row>       
+            </CardBody>
+            </Card>
+        </Col>
+        <Col sm="4">
+            <h3>
+            Select Payment Method
+            </h3>
+            <Card>
+                <CardBody className="paymentCard">
+                    <Card className="p-2 m-2">
+                    <i className="fas fa-address-card"></i>
+                        Net Banking
+                    </Card>
+                    <Card className="p-2 m-2">
+                    <i className="fas fa-address-card"></i>
+                        Net Banking
+                    </Card>
+                    <hr></hr>
+                    <p>Total Amount :</p>
+                    <button type="button" className=" paymentButton btn btn-secondary">Make Payment</button>
+                    </CardBody>
+            </Card>
+        </Col>
+        </Row>
+        <Row className="orderDetailCartRow">
+            <Col sm="8">
+                <h3>
+                    Cart Summary
+                </h3>
+                <Card>
+                <CardBody className="paymentCard">
+                    hello
+                </CardBody>
+            </Card>
+            </Col>
+        </Row>
+    </div>
+  )
+}
+
+export default OrderDetail
